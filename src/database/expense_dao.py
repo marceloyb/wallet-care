f01e8356f0c3288cb8b21055448df3a392392a86
Marcelo <marcelobenesciutti@gmail.com>
2019-12-02 21:47:24 -0300

    expense dao

--- src/database/expense_dao.py
+++ src/database/expense_dao.py
@@ -1,9 +1,25 @@
 from pymongo import MongoClient
+from mongoengine import connect
+from expense import Expense as Model
 
-client = MongoClient('mongodb://127.0.0.1:27017/wallet-care')
-db = client.wallet_care
+class ExpenseDao():
 
-# descobrir como dar import nas configs do banco
+    client = MongoClient('mongodb://127.0.0.1:27017/wallet-care')
+    db = client.wallet_care
+    collection = db.expense
+    connect('wallet_care')
 
-def insert(json):
-    db.expense.insert_one(json)
+    def insert(json):
+        entity = Model()
+        entity.value = json['value']
+        entity.date = json['date']
+        entity.category = json['category']
+        entity.comment = json['comment']
+        result = entity.save()
+        return {'id': str(result.id), 'category': result.category}
+
+    def remove(json):
+        db.expense.delete_one(json)
+
+    def find():
+        return db.expense.find()
